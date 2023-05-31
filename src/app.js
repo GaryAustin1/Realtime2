a866bd8b067439a26634b9fab609c2c7a8d32d02
GaryAustin1 <54564956+GaryAustin1@users.noreply.github.com>
2023-05-30 19:45:02 -0500

    Starting to make more modular and working on visibilty handler for errors.

--- src/app.js
+++ src/app.js
@@ -3,16 +3,15 @@
 async function app () {
     let supaclient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
 
-    const primaryCol = "id"  // would need changes for composite primary
-    const table = "realtest"
 
+   //  NEXT SECTION IS JUST FOR TESTING
     // this is a loop to generate 20 updates or inserts for testing while the subscription is setup
     // The result should be a table with 20 rows all having after-x for message
     // First set table to before for multiple runs
-    const {data:data1,error:error1} = await supaclient.from(table).delete().not('id','is',null)
+    const {data:data1,error:error1} = await supaclient.from('realtest').delete().not('id','is',null)
     console.log(data1,error1)
     for (let i=1; i <= 20; i++ ) {
-        const {data:data2,error:error2} = await supaclient.from(table).insert({id:i,message:'before'+i})
+        const {data:data2,error:error2} = await supaclient.from('realtest').insert({id:i,message:'before'+i})
     }
 
     console.log('table initialized')
@@ -21,8 +20,8 @@ async function app () {
     let interval = setInterval(function() {
         if (i <= 20) {
             console.log('update',i)
-            supaclient.from(table).insert({id:i+20,message:'insert'+(i+20)}).then()  // add 20 new rows
-            supaclient.from(table).update({message: 'after' + i}).eq(primaryCol, i).then()
+            supaclient.from('realtest').insert({id:i+20,message:'insert'+(i+20)}).then()  // add 20 new rows
+            supaclient.from('realtest').update({message: 'after' + i}).eq('id', i).then()
             i++
         }
         else {
@@ -33,45 +32,52 @@ async function app () {
     // End of test setup
 
     console.log('start subscription')
-
-    let eventQueue = []     // holds realtime events before initial table load
-    let memoryTable = []    // The table of data we want to keep refreshed
-    let initMemoryTable = false    // Don't need to queue anymore when true
-    let connected = false     // The postgres_changes event fires continuously so need flag
-
-    const mySubscription = supaclient
-        .channel('myChannel')
-        .on('postgres_changes',
-            { event: '*', schema: 'public', table: table,}, (payload) => {
-                console.log('event, ',payload.eventType,payload.new.id)
-                if (initMemoryTable)
-                    handleEvent(payload)  // running normally
-                else
-                    eventQueue.unshift(payload)  // insert for later
-            })
-        .subscribe((status)=>{
-            console.log('subscribe_status, '+status);
-        })
-        .on('system', {}, payload => {                // need this to know when REALLY connected
-            if (payload.extension === 'postgres_changes') {
-                if (!connected) {
-                    console.log('postgres_changes received, load initial data')
-                    connected = true
-                    // load initial data
-                    supaclient.from(table).select('*').order(primaryCol).then(result=>{
-                        console.log('initial data loaded',result)
-                        //merge previous events into data table
-                        eventQueue.forEach((row)=> {
-                            handleEvent(row)
-                        })
-                        memoryTable = result.data
-                        initMemoryTable = true
-                    })
+    async function startStream(memoryTable,tableName,primaryCol,eventHandler) {
+        console.log('startStream',table)
+        let eventQueue = []     // holds realtime events before initial table load
+        let initMemoryTable = false    // Don't need to queue anymore when true
+        let connected = false     // The postgres_changes event fires continuously so need flag
+        let visible = true
+        let restart = false
+        let mySubscription = supaclient
+            .channel('myChannel')
+            .on('postgres_changes',
+                {event: '*', schema: 'public', table: tableName,}, (payload) => {
+                    console.log('event, ', payload.eventType, payload.new.id)
+                    if (initMemoryTable)
+                        handleEvent(payload,memoryTable,primaryCol)  // running normally
+                    else
+                        eventQueue.unshift(payload)  // insert for later
+                })
+            .subscribe((status) => {
+                console.log('subscribe_status, ' + status);
+                if (status !== 'SUBSCRIBED') {
+                    connectionErrorHandler(status)
                 }
-            }
-        })
-    function handleEvent(payload) {
-        console.log('handleEvent', payload.eventType,payload.new.id)
+            })
+            .on('system', {}, payload => {                // need this to know when REALLY connected
+                if (payload.extension === 'postgres_changes') {
+                    if (!connected) {
+                        console.log('postgres_changes received, load initial data')
+                        connected = true
+                        // load initial data
+                        supaclient.from(tableName).select('*').order(primaryCol).then(result => {
+                            console.log('initial data loaded', result)
+                            memoryTable.push(...result.data)
+                            //merge previous events into data table
+                            eventQueue.forEach((row) => {
+                                handleEvent(row,memoryTable,primaryCol)
+                            })
+                            initMemoryTable = true
+                        })
+                    }
+                }
+            })
+    }
+
+    //eventHandler is separate as it might be different based on insert/update/delete pattern desired
+    function handleEvent(payload,memoryTable,primaryCol) {
+        console.log('handleEvent', payload.eventType, payload.new.id,primaryCol)
         switch (payload.eventType) {
             case "INSERT":   //Doing an "upsert" to handle inserting to existing ids from the eventqueue
                 let objIndex1 = memoryTable.findIndex((obj => obj[primaryCol] === payload.new[primaryCol]));
@@ -91,19 +97,51 @@ async function app () {
                     memoryTable.splice(objIndex3, 1)
                 break
         }
-
     }
+
+    let testTable = []
+    const pCol = 'id'  // would need changes for composite primary
+    const table = "realtest"
+    startStream(testTable,table,pCol,handleEvent)
+
+
+    async function start_up() {
+        console.log('start status',mySubscription,mySubscription.state,restart,document.visibilityState)
+        if (document.visibilityState === 'visible' && restart) {
+            mySubscription.subscribe()
+        }
+    }
+
+    async function connectionErrorHandler (status) {
+        restart = true
+        if (status !== 'CLOSED') mySubscription.unsubscribe()
+        console.log('disconnect',status,mySubscription)
+        if (document.visibilityState === 'visible') start_up()
+        else {
+
+        }
+    }
+
+    document.onvisibilitychange = () => {
+        console.log('visibility change',document.visibilityState)
+        start_up()
+    };
 
 // just to show final result
     setTimeout(function(){
-        console.log('table after ', memoryTable)
-        supaclient.removeChannel(mySubscription)
+        console.log('table after ', testTable)
+        //supaclient.removeChannel(mySubscription)
+
     }, 2000)
 
 }
 document.addEventListener("DOMContentLoaded", function(event) {
     app()
 })
+
+
+
+
 
 let startTime = Date.now()
 function logToHTML (message) {
